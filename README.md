ee85f18e61494c87d68f98bc27b45a814e8f3f28
nuongnguyen <nuonght3599@gmail.com>
2019-05-26 23:38:13 +0700

    Init Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Python-Flask-Demo
+Practise base on https://cloud.google.com/python/getting-started/tutorial-app
+Improvement:
+- Clean for just using mongodb
+- Support select type when adding book
+- Check edit permission
+- Validate title
+
+Link to review: http://python-flask-demo-241716.appspot.com/books/
--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 # The secret key is used by Flask to encrypt session cookies.
 # [START secret_key]
-SECRET_KEY = '\x14\x801\xd2\xd5\xbb\xdb^\x11\x1f\xd8\x1f\xfc\x07\xb0\x1ar\xefh-T\xf2\xe2h'
+SECRET_KEY = 'my_key'
 # [END secret_key]
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'python-flask-demo-241716'
+PROJECT_ID = 'my_project_id'
 
-MONGO_URI = 'mongodb://nuongnguyen_user:jxCAj1TAql9N3Knx@cluster0-shard-00-00-fsk2j.mongodb.net:27017,cluster0-shard-00-01-fsk2j.mongodb.net:27017,cluster0-shard-00-02-fsk2j.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+MONGO_URI = 'mongo_db_uri'
 
 # Google Cloud Storage and upload settings.
 # Typically, you'll name your bucket the same as your project. To create a
@@ -24,7 +24,7 @@ MONGO_URI = 'mongodb://nuongnguyen_user:jxCAj1TAql9N3Knx@cluster0-shard-00-00-fs
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'python-flask-demo-storage'
+CLOUD_STORAGE_BUCKET = 'storage_name'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -40,5 +40,5 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 # If you receive a invalid redirect URI error review you settings to ensure
 # that the current URI is allowed.
 GOOGLE_OAUTH2_CLIENT_ID = \
-    '1091996557552-au1vmj54cvjts2u0454u16alo5n6hn67.apps.googleusercontent.com'
-GOOGLE_OAUTH2_CLIENT_SECRET = '1q6NqNdEYDKo33GkNIcIfjsk'
+    'key'
+GOOGLE_OAUTH2_CLIENT_SECRET = 'private_key'
